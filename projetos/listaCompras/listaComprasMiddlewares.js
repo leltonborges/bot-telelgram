57d4e666972a8fdf12a6d2d853e708388a14fc40
Lelton Borges <lelton.borges1@gmail.com>
2020-07-19 00:59:06 -0300

    Lista Compras (Middleware)

--- projetos/listaCompras/listaComprasMiddlewares.js
+++ projetos/listaCompras/listaComprasMiddlewares.js
@@ -8,29 +8,46 @@ const bot = new Telegraf(env.token)
 const botoes = lista => Extra.markup(
     Markup.inlineKeyboard(
         lista.map(item => Markup.callbackButton(item, `delete ${item}`))
-        , {columns: 3}
+        , { columns: 3 }
     )
 )
 
 bot.use(session())
 
-bot.start(async ctx => {
+const verificarUsuario = (ctx, next) => {
+    const mesmoIDMsg = ctx.update.message
+        && ctx.update.message.from.id === env.userID
+    const mesmoIDCallback = ctx.update.callback_query
+        && ctx.update.callback_query.from.id === env.userID
+
+    if (mesmoIDMsg || mesmoIDCallback) {
+        next()
+    } else {
+        ctx.reply('Desculpe, não fui autorizado a conversar com você...')
+    }
+}
+
+// Vc pode ter mais de um middleware...
+const processando = ({ reply }, next) =>
+    reply('processando...').then(() => next())
+
+bot.start(verificarUsuario, async ctx => {
     const name = ctx.update.message.from.first_name
     await ctx.reply(`Seja bem vindo, ${name}!`)
     await ctx.reply('Escreva os itens que você deseja adicionar...')
     ctx.session.lista = []
 })
 
-bot.on('text', ctx => {
+bot.on('text', verificarUsuario, processando, ctx => {
     let msg = ctx.update.message.text
     ctx.session.lista.push(msg)
     ctx.reply(`${msg} adicionado!`, botoes(ctx.session.lista))
 })
 
-bot.action(/delete (.+)/, ctx => {
+bot.action(/delete (.+)/, verificarUsuario, ctx => {
     ctx.session.lista = ctx.session.lista.filter(
         item => item !== ctx.match[1])
-    ctx.reply(`${ctx.match[1]} deletado!`, botoes(ctx.session.lista))
+    ctx.answerCbQuery(`${ctx.match[1]} deletado!`, botoes(ctx.session.lista))
 })
 
 bot.startPolling()
